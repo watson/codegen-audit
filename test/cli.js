1125236b9dc6ab402e0011cfa7a74feae7bf622a
Thomas Watson <w@tson.dk>
2021-01-07 21:42:32 +0100

    Fix tests to work on Windows

--- test/cli.js
+++ test/cli.js
@@ -1,20 +1,57 @@
 'use strict'
 
 const { join } = require('path')
-const { exec } = require('child_process')
+const { spawn } = require('child_process')
 const { readFile } = require('fs')
 
 const test = require('tape')
 
-const envs = [
-  {},
-  { CI_SIMULATE_SIGINT: 'true' }
-]
+const envs = [{}]
+if (process.platform !== 'win32') {
+  envs.push({ CI_SIMULATE_SIGINT: 'true' })
+}
+
+function run (cmd, args, opts, cb) {
+  if (typeof opts === 'function') {
+    cb = opts
+    opts = {}
+  }
+
+  let stdout = ''
+  let stderr = ''
+  let err = null
+  let code = null
+  let signal = null
+
+  const cp = spawn(cmd, args, opts)
+
+  cp.stdout.on('data', (data) => { stdout += data })
+  cp.stderr.on('data', (data) => { stderr += data })
+  cp.once('error', onError)
+  cp.once('exit', onExit)
+
+  function onError (_err) {
+    err = _err
+    cp.removeListener('exit', onExit)
+    done()
+  }
+
+  function onExit (_code, _signal) {
+    code = _code
+    signal = _signal
+    cp.removeListener('error', onError)
+    done()
+  }
+
+  function done () {
+    cb(err, stdout, stderr, code, signal)
+  }
+}
 
 envs.forEach((env) => {
   test('example-app.js', (t) => {
     t.test('setup', (t) => {
-      exec('rm -f test.json', (err) => {
+      run('rm', ['-f', 'test.json'], (err) => {
         t.error(err)
         t.end()
       })
@@ -34,9 +71,23 @@ envs.forEach((env) => {
         expectedOutput.push('kill: SIGINT')
       }
 
+      const args = [
+        join('lib', 'cli.js'),
+        '--report',
+        'test.json',
+        process.argv0,
+        join('test', 'example-app.js')
+      ]
       const opts = { env: Object.assign({}, process.env, env) }
 
-      exec('node lib/cli.js --report test.json node test/example-app.js', opts, (err, stdout, stderr) => {
+      run(process.argv0, args, opts, (err, stdout, stderr, code, signal) => {
+        // process._rawDebug(
+        //   `-- ERROR START:\n${require('util').inspect(err)}\n-- ERROR END\n` +
+        //   `-- STDOUT START:\n${stdout}\n-- STDOUT END\n` +
+        //   `-- STDERR START:\n${stderr}\n-- STDERR END\n` +
+        //   `-- CODE START:\n${code}\n-- CODE END\n` +
+        //   `-- SIGNAL START:\n${signal}\n-- SIGNAL END`
+        // )
         t.error(err)
         t.deepEqual(stdout.split('\n').sort(), expectedOutput.sort())
         t.equal(stderr, '')
@@ -83,20 +134,38 @@ envs.forEach((env) => {
 })
 
 test('throw', (t) => {
-  exec('node lib/cli.js --report test/example-app-allowlist-fail.json --throw -- node test/example-app.js', (err, stdout, stderr) => {
-    t.ok(err.message.includes('Illegal call to \'Function\' at Object.<anonymous> (test/example-app.js:5:1)'))
-    t.equal(err.killed, false)
-    t.equal(err.code, 1)
-    t.equal(err.signal, null)
+  const args = [
+    join('lib', 'cli.js'),
+    '--report',
+    join('test', 'example-app-allowlist-fail.json'),
+    '--throw',
+    '--',
+    process.argv0,
+    join('test', 'example-app.js')
+  ]
+
+  run(process.argv0, args, (err, stdout, stderr, code, signal) => {
+    t.error(err)
+    t.ok(stderr.includes('Illegal call to \'Function\' at Object.<anonymous> (test/example-app.js:5:1)'))
+    t.equal(code, 1)
+    t.equal(signal, null)
     t.equal(stdout, 'hello from eval\n')
-    const startOfSecondLine = err.toString().indexOf('\n') + 1
-    t.equal(stderr, err.toString().substr(startOfSecondLine))
     t.end()
   })
 })
 
 test('does not throw', (t) => {
-  exec('node lib/cli.js --report test/example-app-allowlist-ok.json --throw -- node test/example-app.js', (err, stdout, stderr) => {
+  const args = [
+    join('lib', 'cli.js'),
+    '--report',
+    join('test', 'example-app-allowlist-ok.json'),
+    '--throw',
+    '--',
+    process.argv0,
+    join('test', 'example-app.js')
+  ]
+
+  run(process.argv0, args, (err, stdout, stderr) => {
     t.error(err)
     t.deepEqual(stdout.split('\n').sort(), [
       'hello from eval',
